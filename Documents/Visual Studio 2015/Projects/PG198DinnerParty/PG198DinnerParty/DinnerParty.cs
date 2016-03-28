c8d903910dc2ffa75ac98d6be1b3791b19fc1be0
leer89 <leer89@gmail.com>
2016-03-28 13:01:17 -0400

    pg 198 - dinner party encapsulation exercises

---
+++ Documents/Visual Studio 2015/Projects/PG198DinnerParty/PG198DinnerParty/DinnerParty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG198DinnerParty
+{
+    class DinnerParty
+    {
+        public int NumberOfPeople;
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations;
+        public const int CostOfFoodPerPerson = 25;
+
+        public void setHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerPerson = 5.00M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20.00M;
+            }
+        }
+
+        public void CalculateCostOfDecorations(bool fancy)
+        {
+            if (fancy)
+            {
+                CostOfDecorations = (NumberOfPeople * 15.00M) + 50M;
+            }
+            else
+            {
+                CostOfDecorations = (NumberOfPeople * 7.50M) + 50M;
+            }
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations + ((CostOfBeveragesPerPerson + CostOfFoodPerPerson) * NumberOfPeople);
+            if (healthyOption)
+            {
+                return totalCost * .95M;
+            }
+            else
+            {
+                return totalCost;
+            }
+        }
+    }
+}
---
+++ Documents/Visual Studio 2015/Projects/PG198DinnerParty/PG198DinnerParty/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PG198DinnerParty
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dinnerParty = new DinnerParty();
+            dinnerParty.NumberOfPeople = 5;
+            dinnerParty.setHealthyOption(false);
+            dinnerParty.CalculateCostOfDecorations(true);
+            DisplayDinnerPartyCost();
+        }   
+
+        private void DisplayDinnerPartyCost() // update dinner cost label
+        {
+            decimal Cost = dinnerParty.CalculateCost(healthyBox.Checked);
+            costLabel.Text = Cost.ToString("c"); // "c" tells ToString() to format as currency value.
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.setHealthyOption(fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.setHealthyOption(healthyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+    }
+}
